5fca75b3bc3a0077085cb53c3be19934729e3c10
JoKer36627 <andrijderkac367@gmail.com>
2025-05-21 15:19:05 +0200

    "Implement Order class with status management"

--- Order.cs
+++ Order.cs
@@ -6,16 +6,27 @@ using System.Threading.Tasks;
 
 namespace Store_simulator
 {
+
+    enum OrderStatus
+    {
+        Created,
+        Paid,
+        Compleated,
+        Cancelled
+    }
     class Order
     {
         public List<Product> Products { get; private set; }
 
         public DateTime OrderDate { get; private set; }
 
+        public OrderStatus Status { get; private set; }
+
         public Order(List<Product> products)
         {
             Products = products ?? new List<Product>();
             OrderDate = DateTime.Now;
+            Status = OrderStatus.Created;
         }
 
         public decimal TotalAmount
@@ -31,12 +42,55 @@ namespace Store_simulator
             return $"Order from {OrderDate}: Total {TotalAmount:C}, Items: {Products.Count}";
         }
 
-        enum OrderStatus
+        public void ChangeStatus(OrderStatus newStatus)
         {
-            Created;
-            Paid;
-            Compleated;
-            Cancelled;
+
+            OrderStatus currentStatus = Status;
+
+            // Check if the new status is valid
+            if (currentStatus == newStatus)
+            {
+                Console.WriteLine("Status is already " + newStatus);
+                return;
+            }
+
+
+            if (currentStatus == OrderStatus.Compleated && newStatus == OrderStatus.Created)
+            {
+                Console.WriteLine("You can't change status from Completed back to Created.");
+                return;
+            }
+
+            if (currentStatus == OrderStatus.Cancelled && newStatus == OrderStatus.Paid)
+            {
+                Console.WriteLine("You can't change status from Cancceled back to Paid.");
+                return;
+            }
+
+
+
+            Status = newStatus;
+
+
+            switch (newStatus)
+            {
+                case OrderStatus.Created:
+                    Console.WriteLine("Order created.");
+                    break;
+
+                case OrderStatus.Paid:
+                    Console.WriteLine("Order Paid.");
+                    break;
+                case OrderStatus.Compleated:
+                    Console.WriteLine("Order completed.");
+                    break;
+                case OrderStatus.Cancelled:
+                    Console.WriteLine("Order cancelled.");
+                    break;
+            }
+
+
+            
         }
 
 
